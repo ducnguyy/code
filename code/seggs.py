51cd5e4f2cc61eddfe381082ddee7e47a956ab96
ducnguyy <minhducnguyen71.12@gmail.com>
2022-09-28 22:54:10 +0700

    update

--- code/seggs.py
+++ code/seggs.py
@@ -71,39 +71,79 @@ class Doggotrainer(torch.nn.Module):
         super(Doggotrainer,self).__init__()
         self.flatten=torch.nn.Flatten()
         self.cnn1=torch.nn.Sequential(
-            torch.nn.Conv2d(3,3,3),
+            torch.nn.Conv2d(250,3,3),
             torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2),
+            #torch.nn.MaxPool2d(2),
         )
         self.cnn2=torch.nn.Sequential(
-            torch.nn.Conv2d(3,20,3),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2),
+            torch.nn.Conv2d(3,20,1),
+            torch.nn.ReLU()
+            #torch.nn.MaxPool2d(2),
         )
         self.fc1=torch.nn.Sequential(
-            #torch.nn.Linear(features),
+            torch.nn.Linear(4960,30),
             torch.nn.ReLU(),
         )
         self.fc2=torch.nn.Sequential(
-            #torch.nn.Linear(features),
+            torch.nn.Linear(30,2),
             torch.nn.Softmax(1),
         )
     def forward(self,x):
         #x=self.flatten(x)
         x=self.cnn1(x)
         x=self.cnn2(x)
-        #x=self.fc1(x)
-        #output=self.fc2(x)
-        #return output
-        return x
+        x=self.flatten(x)
+        x=self.fc1(x)
+        output=self.fc2(x)
+        return output
+        #return x
+TRAINER=Doggotrainer().float()
+loss_criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(TRAINER.parameters(),lr=0.1)
 
-height,weight= 250,250
-n_epochs=10
-loss=torch.nn.BCELoss()
-cattomodel=Doggotrainer()#.to(device)
-print(cattomodel)
-YE=torch.rand(1,3,250,250)
-logits=cattomodel(YE)
-pred_probab = torch.nn.Softmax(dim=1)(logits)
-y_pred = pred_probab.argmax(1)
-print(f"Predicted class: {y_pred}")
+
+for epoch in range(30):
+    for data, target in dataset_loader:
+#         data = np.array(data)
+#         data = torch.from_numpy(data)
+        data = data.permute(0,3,2,1)
+        
+        score = TRAINER(data.float())
+#         print(target.shape, score.shape, data.shape, target.shape)
+        
+        loss = loss_criterion(score, target.long())
+        
+        optimizer.zero_grad()
+        
+        loss.backward()
+        
+        optimizer.step()
+    
+print(f"Loss for current epoch {epoch} : {loss}")
+def check_accuracy(model, loader):
+    model.eval()
+    
+    total_correct = 0
+    total_predictions = 0
+    
+    with torch.no_grad():
+        for x,y in loader:
+            #x = x.to(device=device)
+#             x = np.array(x)
+#             x = torch.from_numpy(x)
+            x = x.permute(0,3,2,1)
+            
+            #y = y.to(device=device)
+            
+            score = model(x.float())
+            
+            _, prediction = score.max(1)
+            
+#             print((y==prediction).sum())
+            total_correct += (y==prediction).sum()
+            total_predictions +=  prediction.size(0)
+
+    TRAINER.train()
+    
+    print(f"Accuracy : {float(total_correct/total_predictions)*100:.2f}")
+check_accuracy(TRAINER, dataset_loader)
